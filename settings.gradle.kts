079fd1ab265637ef5cdc951a43c57783b58e6267
gadgeski <xtwh.ce5bil@gmail.com>
2026-01-06 23:38:55 +0900

    fix: Remove foojay-resolver plugin to fix CI build errors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    // Javaツールチェーンの自動解決プラグイン (CI等でJDKを自動セットアップするのに役立ちます)
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
